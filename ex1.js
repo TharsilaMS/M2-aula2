5412fb92b3688dfc981028d37e562400826f38b1
TharsilaMS <130008581+TharsilaMS@users.noreply.github.com>
2023-09-15 21:24:18 -0300

    EX1

---
+++ ex1.js
@@ -0,0 +1,13 @@
+const filmes = ['A freira', 'corra', 'O pacto'];
+console.log(filmes[0])
+
+const dataNc = 2003;
+const dataAtual = 2023;
+const idade = dataAtual- dataNc;
+console.log(` Sua idade é ${idade} anos`)
+
+const musica = ['Shirt', 'Cruel Summer', 'Into you'];
+for (musicas of musica) {
+  musica.push('')
+}
+console.log(musica)
